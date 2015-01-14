2e1d51ffb0df5b27df8e1e57a3410529cb722d0b
Aram Zucker-Scharff <azuckers@gmu.edu>
2015-01-14 15:49:16 -0500

    Woooo things that work.

--- index.php
+++ index.php
@@ -15,21 +15,24 @@ function make_array_url_q($array){
 
 
 # Going to sub in my own cURL stuff here. 
-function get_url($base_url, $file, $vars, $headers, $method = 'GET'){
+function get_url($base_url, $file, $vars, $headers = array('Api-User-Agent: Example/1.0'), $method = 'GET'){
 
-    $url_qs = make_array_url_q($vars);
-    $ch = curl_init();
+    if (is_array($vars)){ $url_qs = make_array_url_q($vars); } else { $url_qs = '?'.$vars; }
+ #   $ch = curl_init();
     #$fp = fopen($file.$url_qs, "w");
     var_dump($base_url.$file.$url_qs);
+#    var_dump(get_headers($base_url.$file.$url_qs));
 #    curl_setopt($ch, CURLOPT_FILE, $fp);
-    curl_setopt($ch, CURLOPT_HEADER, 0);
-    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
-    curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch,CURLOPT_URL,$base_url.$file.$url_qs);
+#    curl_setopt($ch, CURLOPT_HEADER, 0);
+#    curl_setopt($ch, CURLOPT_USERAGENT, 'MyCoolTool/1.1 (http://example.com/MyCoolTool/; MyCoolTool@example.com) BasedOnSuperLib/1.4');
+#    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
+#    curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
+#    curl_setopt($ch,CURLOPT_URL,$base_url.$file.$url_qs);
     #curl_setopt($ch,CURLOPT_HTTPHEADER,$headers); #curl_setopt($ch,CURLOPT_HTTPHEADER,array('HeaderName: HeaderValue'));
 
-    $r = curl_exec($ch);
-    curl_close($ch);
+#    $r = curl_exec($ch);
+    $r = file_get_contents($base_url.$file.$url_qs);
+#    curl_close($ch);
 #    fclose($fp);
     return $r;
 
@@ -37,11 +40,20 @@ function get_url($base_url, $file, $vars, $headers, $method = 'GET'){
 
 function get_wikidata_url($query_array){
 
+    #https://www.wikidata.org/w/api.php?action=help&modules=query
     $base_url = "http://wikidata.org/";
     $file = "w/api.php";
     $query = $query_array;
-
-    $r = get_url($base_url,$file,$query);
+    $un = "nypl-zs";
+    $pw = "npyppplp-pzpsp";
+    $login_q = array(
+                    'action' => 'login',
+                    'lgname' => $un,
+                    'lgpassword' => $pw
+                );
+    $l = get_url("http://mediawiki.org/",$file,$login_q,'','POST');
+    $r = get_url($base_url,$file,$query,'','POST');
+    return $r;
 }
 
 $file = file_get_contents('small_metadata.json');
@@ -76,8 +88,14 @@ $obj_file = json_decode($file);
 **/
 $url = 'https://wdq.wmflabs.org/';
 $file = 'api';
-$vars = array('q' => 'CLAIM[31:188451]');
-var_dump(get_url($url,$file,$vars,'')); die();
+$vars = array(
+            'action' => 'wbgetentities',
+            'titles' => 'Pizza',
+            'sites'  => 'enwiki',
+            'format' => 'json'
+            );
+#$vars = 'action=wbgetentities&sites=frwiki&titles=France&languages=zh-hans|zh-hant|fr&props=sitelinks|labels|aliases|descriptions&format=json';
+var_dump(get_wikidata_url($vars)); die();
 
 foreach ($obj_file->files as $book) {
     ?><p><?php
